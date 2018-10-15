61999e07da60e5819e7de60ad07ba9572754542a
akash <akash@localhost.localdomain>
2018-10-16 00:14:55 +0530

    Changes in URL

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 //'mongodb://localhost:27017/MeanStack'
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://akyi0304:akyi9999@ds133353.mlab.com:33353/mean_stack_db', {
 	useNewUrlParser: true
 }, (err) => {
 	if (err) {
