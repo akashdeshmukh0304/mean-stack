9ad11c449fd2f4329e4666758657dd58155f0b47
akash <akash@localhost.localdomain>
2018-10-16 00:07:22 +0530

    Changes in mongo db URI

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/MeanStack', {
+//'mongodb://localhost:27017/MeanStack'
+
+mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true
 }, (err) => {
 	if (err) {
